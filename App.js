6c11d4c439c80fb839654ae72fae52fef8fdd980
chi_Script <chinedu.onyeoka@gmail.comgit config --global user.name chibaba>
2018-03-06 15:40:58 +0100

    connected to react-native

--- App.js
+++ App.js
@@ -9,63 +9,33 @@ import PlaceList from "./src/components/PlaceList/PlaceList";
 //import PlaceImage from "./src/assets/preety.png";
 
  class App extends Component {
-  state = {
-    places: [],
-    selectedPlace: null
-  };
-
-  placeAddedHandler = placeName => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.concat({
-          key: Math.random(),
-          name: placeName,
-          image: {
-            uri: "https://www.pexels.com/search/nature/"
-          }
-        })
-      };
-    });
+    placeAddedHandler = placeName => {
+    this.props.onAddPlace(placeName);
   };
 
   placeDeletedHandler = () => {
-    this.setState(prevState => {
-      return {
-        places: prevState.places.filter(place => {
-          return place.key !== prevSate.selectedPlace.key;
-        }),
-        selectedPlace: null
-      };
-    });
+    this.props.onDeletePlace();
   };
 
   modalClosedHandler = () => {
-    this.setStae({
-      selectedPlace: null
-    });
+    this.props.onDeselectPlace();
   };
 
   placeSelectedHandler = key => {
-    this.setState(prevState => {
-      return {
-        selectedPlace: prevState.places.find(place => {
-          return place.key === key;
-        })
-      };
-    });
+    this.props.onSelectedPlace(key);
   };
 
   render() {
     return (
       <View style={styles.container}>
         <PlaceDetail
-          selectedPlace={this.state.selectedPlace}
+          selectedPlace={this.props.selectedPlace}
           onItemDeleted={this.placeDeletedHandler}
           onModalClosed={this.modalClosedHandler}
         />
         <PlaceInput onPlaceAdded={this.placeAddedHandler} />
         <PlaceList
-          places={this.state.places}
+          places={this.props.places}
           onItemSelected={this.placeSelectedHandler}
         />
       </View>
@@ -93,9 +63,9 @@ const mapStateToProps = state => {
 const mapsDispatchToProps = dispatch => {
   return {
     onAddPlace: () => dispatch ( addPlace(name)),
-    ondeletePlace: () => dispatch( deletePlace()),
-    onselectedPlace: (key) => dispatch ( selectPlace (key)),
-    ondeselectPlace: () => dispatch ( onseleectPlace())
+    onDeletePlace: () => dispatch( deletePlace()),
+    onSelectedPlace: (key) => dispatch ( selectPlace (key)),
+    onDeselectPlace: () => dispatch ( onseleectPlace())
   }
 }
 export default connect( mapsStateToProps, mapsDispatchToProps)(App)
